fbf72b55cf11c6a3cbf2ac95f31fa330cad99286
Student <Student@Train11-5-PC119.control.ad.sit.kmutt.ac.th>
2025-07-22 15:25:33 +0700

    Add class2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # BasicP_Sunday
+# Sunday 68130500012
---
+++ class2.py
@@ -0,0 +1,58 @@
+# # age =  20
+# # print(age)
+
+# num = 5
+# print(type(num))
+
+# num2 = 1.1
+# print(type(num2))
+
+# boolean = True
+# print(type(boolean))
+
+# name = "Sunday 1 2 123"
+# print(type(name))
+# print(name)
+
+# age = "20"
+# print(type(age))
+
+# age2 = int(age)
+# print(type(age2))
+
+# num1 = 5
+# print(type(num1))
+
+# num2 = float(num1)
+# print(type(num2))
+# print(num2)
+
+# age = 20.5
+# print(type(age))
+
+# age2 = str(age)
+# print(type(age2))
+
+
+# tf = True
+# print(type(tf))
+
+# tf2 = str(tf)
+# print(type(tf2)
+  
+
+# sum =  "str" <  "strr" 
+# print(sum)
+
+# age = int(input("Enter your age ."))
+# # print("You age is",age)
+# print(type(age))
+
+# age = int(input("Enter your age. "))
+
+# if age > 18:
+#     print("Your age more than 18")
+# elif age > 20:
+#     print("Your age more than 15.")
+# else:
+#     print("You age lower than 18")
