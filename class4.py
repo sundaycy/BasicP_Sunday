dcc54a9020e9900e972d1ee52891ee88b6e675ee
sundaycy <Chayuth.yo@gmail.com>
2025-07-24 12:30:18 +0700

    Add files via upload

---
+++ class4.py
@@ -0,0 +1,78 @@
+# def grade(score):
+#     if score >= 80:
+#         print("เกรด A")
+#     elif score >=70 :
+#         print("เกรด B")
+#     elif score >= 60 :
+#         print("เกรด C")
+
+# grade(60)
+
+# def my_sum(a,b):
+#     return a+b
+
+# result = my_sum(5,5)
+# print(result)
+
+# def menu(a,b):
+#     cho = int(input("เลือกเมนู 1.GO 2.Leave"))
+#     if cho == 1:
+#         print("ใส่ราคาสินค้า")
+#     if cho == 2:
+#         print("bye")    
+# menu(1,2)
+
+# def sumation(quan,per):
+#     result = quan * per 
+#     total_vat = result * 0.07
+#     total = result + total_vat
+#     return total
+
+# def menu():
+#     while True:
+#         choice = int(input("1.คำนวณสินค้า , 2.ออก"))
+#         if choice == 1:
+#             quan = int(input("จำนวนสินค้าทั้งหมด : "))
+#             per = int(input("ราคาต่อชิ้น :"))
+#             print(sumation(quan,per))
+
+#         if choice == 2:
+#             print("Bye")
+#             break
+# menu()
+
+# my_list = [1,2,"Day"]
+
+# # result = my_list[0]
+# # print("Before",result)
+# # my_list[0] = 3
+# # print("After",my_list[0])
+
+# print("Before",my_list)
+
+# # my_list.append("GGG")
+# my_list.pop(1)
+
+# print("After",my_list)
+
+# my_list = [1,2,"day"]
+
+# for i in my_list:
+#     print(i)
+
+# my_dict = {"name":"day", "age": 18}
+# print("Before",my_dict)
+# my_dict ["iT"] = 30
+# print("After",my_dict)
+
+# students = [
+#     {"name" : "day","age": 18},
+#     {"name" : "sun","age": 18}
+# ]
+# for i in students:
+#     print(i["age"])
+
+
+    movie_list = [
+        {"name": "super"}
+    ]
